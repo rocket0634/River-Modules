948cbd3878a30f7b9720cf5db7f1f025a3693740
Riverbui <4911928+rocket0634@users.noreply.github.com>
2018-07-10 01:31:00 -0700

    Idea dump and add Pressing Down

---
+++ Down/Assets/Down.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KMHelper;
+
+public class Down : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    public KMSelectable buttonLeft, buttonUp, buttonRight;
+    public Texture2D cat, ledge;
+    public TextMesh Commands;
+    public TextMesh Names;
+    public KMModSettings modSettings;
+    public Renderer textRenderer;
+    private List<string> defaultNames = new List<string>();
+    private List<string> defaultCommands = new List<string>();
+
+    private static int moduleIdCounter = 1;
+    private int moduleId;
+    private bool Active = false;
+    private bool Solved = false;
+    private int strikeGet;
+
+    void Start()
+    {
+        moduleId = moduleIdCounter++;
+        Module.OnActivate += delegate() { Active = true; };
+
+        buttonLeft.OnInteract += delegate { ButtonPress(buttonLeft); return false; };
+        buttonRight.OnInteract += delegate { ButtonPress(buttonRight); return false; };
+        buttonUp.OnInteract += delegate { ButtonPress(buttonUp); return false; };
+    }
+
+    private void Database()
+    {
+        
+    }
+
+    void Update()
+    {
+        strikeGet = Bomb.GetStrikes();
+    }
+
+    private bool ButtonPress(KMSelectable button)
+    {
+        if (Active && !Solved)
+        {
+            button.AddInteractionPunch();
+            Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+            if (button == buttonUp)
+            {
+                Module.HandlePass();
+                Solved = true;
+                return true;
+            }
+            else if (button == buttonLeft)
+            {
+                Debug.LogFormat("{0}", strikeGet.ToString());
+            }
+            else
+            {
+                Module.HandleStrike();
+            }
+        }
+        return false;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # River-Modules
+This is a repository consisting of module ideas I have come up with at one point and may continue on my own. With my current inavailability with internet access, I've decided to leave these ideas out in the open in case anyone would like to get to them before I'm able to.
+
+Note, that while I am away, suggestions and PRs are allowed, and I'll look at them at my earliest convenience.
